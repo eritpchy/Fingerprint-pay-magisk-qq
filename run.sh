9f3527b140bd4d3f73d3d936721e9b56a8aadcea
Jason Eric <eritpchy@qq.com>
2020-01-11 12:03:08 +0800

    + 3.9.6 适配 Magisk 20.2

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-./gradlew clean && ./gradlew :riru-module-xfingerprint-pay-qq:zip && adb push ./release/* /sdcard/Download/
+#!/bin/bash
+set -e
+cd ${0%/*}
+rm -rf ./out/magisk_module
+./gradlew clean
+./gradlew :module:assembleRelease
+adb shell rm -fv "/data/local/tmp/libxfingerprint_pay_qq.dex"
+adb push ./out/*.zip /sdcard/Download/
