e65275e23f31b971c918a3d5d7e98f4afca02142
Jason Eric <eritpchy@qq.com>
2020-05-02 18:18:47 +0800

    + 3.9.10 适配 Magisk 20.4

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 cd ${0%/*}
-rm -rf ./out/magisk_module
 ./gradlew clean
 ./gradlew :module:assembleRelease
-adb shell rm -fv "/data/local/tmp/libxfingerprint_pay_qq.dex"
+adb shell rm -f "/data/local/tmp/libriru_module_xfingerprint_pay_qq.dex"
 adb push ./out/*.zip /sdcard/Download/
